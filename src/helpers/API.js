da68232191b7b162fffac116bdfa7948faaabd55
calimits <calimitsacd@gmail.com>
2025-07-19 09:59:50 -0400

    haciendo deploy con gh-pages

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,26 +1,27 @@
 
-const domainName = "localhost:3000";
+//https://certificate-printer-server.onrender.com
+const urlAndDomainName = "https://certificate-printer-server.onrender.com";
 const URLs = {
-        getUser: `http://${domainName}/api-cert-printer/users`,
-        getAllUsers: `http://${domainName}/api-cert-printer/users`,
-        login: `http://${domainName}/api-cert-printer/login/users`,
-        postUser: `http://${domainName}/api-cert-printer/users`,
-        putUser: `http://${domainName}/api-cert-printer/users`,
-        delUser: `http://${domainName}/api-cert-printer/users`,
-        checkUser: `http://${domainName}/api-cert-printer/check`,
-        getCert: `http://${domainName}/api-cert-printer/certificates`,
-        getAllCerts: `http://${domainName}/api-cert-printer/certificates`,
-        searchCerts: `http://${domainName}/api-cert-printer/search/certificates`,
-        getUserCerts: `http://${domainName}/api-cert-printer/certificates/users`,
-        postCerts: `http://${domainName}/api-cert-printer/certificates`,
-        putCert: `http://${domainName}/api-cert-printer/certificates`,
-        delCert: `http://${domainName}/api-cert-printer/certificates`,
-        delUserCerts: `http://${domainName}/api-cert-printer/delete/certificates`,
-        delAllCerts: `http://${domainName}/api-cert-printer/certificates`
+        getUser: `${urlAndDomainName}/api-cert-printer/users`,
+        getAllUsers: `${urlAndDomainName}/api-cert-printer/users`,
+        login: `${urlAndDomainName}/api-cert-printer/login/users`,
+        postUser: `${urlAndDomainName}/api-cert-printer/users`,
+        putUser: `${urlAndDomainName}/api-cert-printer/users`,
+        delUser: `${urlAndDomainName}/api-cert-printer/users`,
+        checkUser: `${urlAndDomainName}/api-cert-printer/check`,
+        getCert: `${urlAndDomainName}/api-cert-printer/certificates`,
+        getAllCerts: `${urlAndDomainName}/api-cert-printer/certificates`,
+        searchCerts: `${urlAndDomainName}/api-cert-printer/search/certificates`,
+        getUserCerts: `${urlAndDomainName}/api-cert-printer/certificates/users`,
+        postCerts: `${urlAndDomainName}/api-cert-printer/certificates`,
+        putCert: `${urlAndDomainName}/api-cert-printer/certificates`,
+        delCert: `${urlAndDomainName}/api-cert-printer/certificates`,
+        delUserCerts: `${urlAndDomainName}/api-cert-printer/delete/certificates`,
+        delAllCerts: `${urlAndDomainName}/api-cert-printer/certificates`
 
 }
 
 export const API = {
-    domainName,
+    urlAndDomainName,
     URLs
 }
